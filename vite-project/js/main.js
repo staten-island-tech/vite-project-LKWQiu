dfb631aef36c04f86e76fcb612642c20c8a5aeaa
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-11-15 09:22:12 -0500

    dsda

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,8 +1,8 @@
-import "./style/style.css";
+import "../css/style.css";
 
 document.querySelector('#app').innerHTML = `
-<h1> Hello VITE!!!</h1>
-<a href="https://vitejs.dev" target="_blank"> Documentation</a>
+  <h1> Hello VITE!!!</h1>
+  <a href="https://vitejs.dev" target="_blank"> Documentation</a>
 `;
 
 
