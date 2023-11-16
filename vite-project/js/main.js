936e59e348c61f1d363206a9b992bac3378a8eef
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-11-16 10:04:13 -0500

    ojiASonSAod

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,14 +1,28 @@
 import "../css/style.css";
 import { assaultRiflesArray } from "../js/AssaultRifles.js";
 
-const
+const DomSelectors = {
+  Content: document.getElementById("Content"),
+}
 
 const printAssaultRifles = 
 assaultRiflesArray.forEach(printAssaultRifles => {
   console.log(printAssaultRifles)
 });
-function displayAssaultRifles (printAssaultRifles){
-  const boxAR = 
-}
+assaultRiflesArray.forEach((rifle) => {
+  const arSet =` 
+  <div class="ARset">
+    <h3>${rifle.name}</h3>
+    <p>Rank Requirement: ${rifle.rankRequirement}</p>
+    <p>Damage: ${rifle.damage}</p>
+    <p>Range: ${rifle.range}</p>
+    <p>Weapon Type: ${rifle.weaponType}</p>
+    <p>Weapon Class: ${rifle.weaponClass}</p>
+    <p>Firing Modes: ${rifle.firingModes.join(", ")}</p>
+    <p>Magazine Size: ${rifle.magazineSize}</p>
+  </div>
+`;
+DomSelectors.Content.insertAdjacentHTML("beforeend", arSet)
+});
 
-
+displayAssaultRifles()
